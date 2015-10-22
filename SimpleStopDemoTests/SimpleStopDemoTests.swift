e5d3a965389e36e08ca7bf59a0b21a9d2719290f
DAWSON ZACHARIAH <737637@ehs-s-itc-23>
2015-10-22 10:44:20 -0500

    Initial Commit

---
+++ SimpleStopDemoTests/SimpleStopDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleStopDemoTests.swift
+//  SimpleStopDemoTests
+//
+//  Created by DAWSON, ZACHARIAH on 10/22/15.
+//  Copyright © 2015 Zach Dawson Inc. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleStopDemo
+
+class SimpleStopDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
